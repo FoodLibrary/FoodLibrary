e30328271049bcc5924701673b1d70487c912bb6
천수빈 <suebin710@gmail.com>
2020-05-03 20:18:37 +0900

    initial settings

--- src/main/foodlibrary-frontend/src/App.js
+++ src/main/foodlibrary-frontend/src/App.js
@@ -1,17 +1,22 @@
-import React, {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useEffect, useState} from "react";
+
 import 'reactstrap';
 import ProductPage from './ProductPage/js/ProductPage';
 
 import TopBar from "./defaultDiv/js/TopBar";
 import SignUp from "./SignUpPage/SignUp";
 import ReviewDetail from "./ReviewDetailPage/js/ReviewDetail";
+import ResultFiltering from "./SearchResultPage/js/ResultFiltering";
+import ProductList from "./SearchResultPage/js/ProductList";
+import SearchResult from "./SearchResultPage/js/SearchResult";
 
 function App() {
     return (
         <div>
             <TopBar/>
-            <ReviewDetail/>
+            <ResultFiltering/>
+            <SearchResult/>
         </div>
     )
 }
