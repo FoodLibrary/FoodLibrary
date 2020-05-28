6f61eade77652df79707c8b0a0f2bb6d6ad6c456
천수빈 <suebin710@gmail.com>
2020-05-29 01:52:05 +0900

    Login Page Renewal

--- src/main/foodlibrary-frontend/src/LoginPage/js/LoginPage.js
+++ src/main/foodlibrary-frontend/src/LoginPage/js/LoginPage.js
@@ -1,17 +1,48 @@
-import React ,{Component} from 'react';
-import LoginPageLogo from './LoginPageLogo';
-import LoginPageFoot from './LoginPageFoot'
+import React, {Component} from 'react';
 import '../css/LoginPage.css';
-import { Container } from 'reactstrap';
+import {Container, Row, Col, NavbarBrand, Input, Button} from 'reactstrap';
 import TopBar from "../../defaultDiv/js/TopBar";
+
+const imageResources = require('../../util/ImageResources');
 
 const LoginPage = () => {
     return (
-      <div className="LoginPage">
-          <TopBar/>
-          <LoginPageLogo/>
-          <LoginPageFoot/>
-      </div>
+        <div className="LoginPage">
+            <TopBar/>
+            <Container>
+                <Row>
+                    <Col xl={1}></Col>
+                    <Col md={{size:2, offset:1}} xl={{size:2, offset:1}}>
+                        <NavbarBrand href="/" className="mr-auto" > <img id={"title"} src={imageResources.logoImg}/> </NavbarBrand>
+                    </Col >
+                    <Col xl={{size:1, offset:2}}></Col>
+                </Row>
+                <Row>
+                    <Col xl={{size:6, offset:3}}>
+                        <Input type="text" placeholder="아이디" id={"loginID"} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xl={{size:6, offset:3}}>
+                        <Input type="password" placeholder="비밀번호" id={"loginPW"} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xl={{size:6, offset:3}}>
+                        <Button id={"loginBtn"} > 로그인 </Button>
+                    </Col>
+                </Row>
+                <Row className={"findIdPw"}>
+                    <Col xl={{size:2, offset:4}}>
+                        <span className={"findIdPw"}> <a href={'/'} > 아이디 찾기 </a> </span>
+                    </Col>
+                    <Col xl={2}>
+                        <span className={"findIdPw"}> <a href={'/'} > 비밀번호 찾기 </a> </span>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
     );
 
 }
--- src/main/foodlibrary-frontend/src/LoginPage/js/LoginPageLogo.js
+++ src/main/foodlibrary-frontend/src/LoginPage/js/LoginPageLogo.js
@@ -2,14 +2,13 @@ import React,{Component} from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import '../css/LoginPageLogo.css';
 
+const imageResources = require('../../util/ImageResources');
+
 const LoginPageLogo = () => {
         return(
             <Container>
                 <Row>
-                    <Col className="one" >
-                        <img id="img" src='https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/86F49F96-783D-4A15-81E8-1DF8BE9138B0.png'/>
-                        <span className="two">음식도서관</span>
-                    </Col>
+                    <Col> Login </Col>
                 </Row>
             </Container>
         );
